46bf5dfec4960a953738a7198fbef8b60aee60ca
tenge646 <144627741+tenge646@users.noreply.github.com>
2023-11-28 21:59:07 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = self.validate_page_count(page_count)
+
+    def validate_page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+            return None
+        return page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
